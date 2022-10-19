faade21f159648af2dd6cf2d1013772f92f83c4e
ArslanAshraf462 <arslan.ashraf.oraxtech@gmail.com>
2022-10-19 15:52:32 +0500

    Designing the login screen

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mvvm_api/utils/routes/routes_name.dart';
 import '../../view/home_screen.dart';
 
-import '../../view/login_screen.dart';
+import '../../view/login_view.dart';
 
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings){
@@ -10,7 +10,7 @@ class Routes {
       case RoutesName.home:
         return MaterialPageRoute(builder: (context) => const HomeScreen(),);
       case RoutesName.login:
-        return MaterialPageRoute(builder: (context) => const LoginScreen(),);
+        return MaterialPageRoute(builder: (context) => const LoginView(),);
         default:
           return MaterialPageRoute(builder: (_) {
             return const Scaffold(
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -5,6 +5,15 @@ import 'package:fluttertoast/fluttertoast.dart';
 
 class Utils{
 
+  static void fieldFocusChange(
+      BuildContext context,
+      FocusNode current,
+      FocusNode nextFocus
+      ){
+    current.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
   static toastMessage(String message){
     Fluttertoast.showToast(
         msg: message,
@@ -13,19 +22,22 @@ class Utils{
     );
   }
   static void flushBarErrorMessage(String message, BuildContext context){
-    Flushbar(
-      message: message,
-      forwardAnimationCurve: Curves.decelerate,
-      margin: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-      padding: const EdgeInsets.all(15),
-      borderRadius: BorderRadius.circular(20),
-      backgroundColor: Colors.red,
-      reverseAnimationCurve: Curves.easeInOut,
-      duration: const Duration(seconds: 3),
-      flushbarPosition: FlushbarPosition.TOP,
-      positionOffset: 20,
-      icon: const Icon(Icons.error, size: 28, color: Colors.white,),
-    ).show(context);
+    showFlushbar(
+        context: context,
+        flushbar: Flushbar(
+          message: message,
+          forwardAnimationCurve: Curves.decelerate,
+          margin: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+          padding: const EdgeInsets.all(15),
+          borderRadius: BorderRadius.circular(20),
+          backgroundColor: Colors.red,
+          reverseAnimationCurve: Curves.easeInOut,
+          duration: const Duration(seconds: 3),
+          flushbarPosition: FlushbarPosition.TOP,
+          positionOffset: 20,
+          icon: const Icon(Icons.error, size: 28, color: Colors.white,),
+        )..show(context));
+
   }
 
   static snackBar(String message, BuildContext context){
